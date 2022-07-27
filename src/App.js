bb81b93d79d0884e20657cf66c974c4b82fc6829
raphael94360 <paulberthet94@gmail.com>
2022-07-27 16:27:19 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -1,53 +1,110 @@
-import "./App.scss";
-import Header from "./components/Header";
-import Section from "./components/Section";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import "./App.scss"
+import Header from "./components/Header"
+import Section from "./components/Section"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 function App() {
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [string, setString] = useState("");
+  const [data, setData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const [cart, setCart] = useState([])
 
   const fetchData = async () => {
-    const response = await axios.get("http://localhost:4000/");
-    setData(response.data);
-    setIsLoading(false);
-  };
+    const response = await axios.get("https://backend-delivero.herokuapp.com/")
+    setData(response.data)
+    setIsLoading(false)
+  }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
+
+  const addToCart = meal => {
+    const copy = [...cart]
+    const existingID = copy.find(element => element.id === meal.id)
+    if (existingID) {
+      existingID.quantity = existingID.quantity + 1
+    } else {
+      const newMeal = { ...meal, quantity: 1 }
+      copy.push(newMeal)
+    }
+    setCart(copy)
+  }
+
+  const removeToCart = meal => {
+    const copy = [...cart]
+    const inCart = copy.find(element => element.id === meal.id)
+    if (inCart.quantity === 1) {
+      const index = copy.indexOf(inCart)
+      copy.splice(index, 1)
+    } else {
+      inCart.quantity = inCart.quantity - 1
+    }
+    setCart(copy)
+  }
+
+  let sousTotal = 0
+  let fraisLivraison = 2.5
 
   return isLoading ? (
     <span> En cours de chargement... </span>
   ) : (
     <>
       <header>
-        <Header
-          name={data.restaurant.name}
-          description={
-            "Profitez de chaque plaisir de la vie quotidienne. Le Pain Quotidien propose des ingrédients simples et sains, du bon pain, des fruits et des légumes frais et de saison issus de l’agriculture biologique."
-          }
-          picture={"https://f.roocdn.com/images/menus/17697/header-image.jpg"}
-        />
+        <Header name={data.restaurant.name} description={"Profitez de chaque plaisir de la vie quotidienne. Le Pain Quotidien propose des ingrédients simples et sains, du bon pain, des fruits et des légumes frais et de saison issus de l’agriculture biologique."} picture={"https://f.roocdn.com/images/menus/17697/header-image.jpg"} />
       </header>
-      <main className='container'>
-        {data.categories.map((categorie, index) => {
-          return (
-            categorie.meals.length > 0 && (
-              <Section
-                categorie={categorie}
-                key={index}
-                setString={setString}
-                string={string}
-              />
+      <main className="container">
+        <div>
+          {data.categories.map((categorie, index) => {
+            return categorie.meals.length > 0 && <Section categorie={categorie} key={index} addToCart={addToCart} />
+          })}
+        </div>
+
+        <aside>
+          <h2> Valider mon panier </h2>
+
+          {cart.map((meal, index) => {
+            sousTotal = sousTotal + meal.quantity * meal.price
+            return (
+              <div key={index} className="panier">
+                <button
+                  onClick={() => {
+                    removeToCart(meal)
+                  }}
+                >
+                  -
+                </button>
+                <span>{meal.quantity}</span>
+                <button
+                  onClick={() => {
+                    addToCart(meal)
+                  }}
+                >
+                  +
+                </button>
+                <h3> {meal.title}</h3>
+                <p>{(meal.price * meal.quantity).toFixed(2)} €</p>
+              </div>
             )
-          );
-        })}
+          })}
+          <div className="total">
+            <p>
+              <span> Sous-total :</span>
+              <span>{sousTotal.toFixed(2)} € </span>
+            </p>
+            <p>
+              <span>Frais de livraison :</span>
+              <span>{fraisLivraison.toFixed(2)} € </span>
+            </p>
+            <p>
+              <span> Total : </span>
+              <span>{(sousTotal + fraisLivraison).toFixed(2)} € </span>
+            </p>
+          </div>
+        </aside>
       </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,47 +1,44 @@
-const Section = ({ categorie, setString }) => {
+const Section = ({ categorie, addToCart }) => {
   return (
     <section>
       <h2>{categorie.name}</h2>
-      <div className='meals-container'>
-        {categorie.meals.map((meal) => {
-          const reduce = (string) => {
-            let result = "";
+      <div className="meals-container">
+        {categorie.meals.map(meal => {
+          const reduce = string => {
+            let result = ""
             for (let i = 0; i < string.length; i++) {
-              result = result + string[i];
+              result = result + string[i]
               if (result.length > 60) {
-                return result;
+                return result
               }
             }
-          };
+          }
 
           return (
-            <div className='meals-content' key={meal.id}>
-              <div className='meals-content--right'>
+            <div
+              className="meals-content"
+              key={meal.id}
+              onClick={() => {
+                addToCart(meal)
+              }}
+            >
+              <div className="meals-content--right">
                 <h3>{meal.title}</h3>
                 <p>
                   {reduce(meal.description)}
-                  <span
-                    onClick={() => {
-                      setString(meal.description);
-                      console.log(meal.description);
-                    }}
-                  >
-                    +
-                  </span>
+                  <span></span>
                 </p>
 
                 <span> {meal.price} € </span>
-                {meal.popular && <span className='popular'>popular</span>}
+                {meal.popular && <span className="popular">popular</span>}
               </div>
-              <div className='meals-content--left'>
-                {meal.picture ? <img src={meal.picture} alt='meal' /> : " "}
-              </div>
+              <div className="meals-content--left">{meal.picture ? <img src={meal.picture} alt="meal" /> : " "}</div>
             </div>
-          );
+          )
         })}
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Section;
+export default Section
